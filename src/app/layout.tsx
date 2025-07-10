45d7e0b1b37217cad257ddbab01c71f7ac7d06e1
Priyanka <priyankakumari@Priyankas-MacBook-Air.local>
2025-07-10 21:40:19 +0200

    Add home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,15 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <AuthProvider>
           <Navbar />
           {children}
           <Toaster />
-        </body>
-      </AuthProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
